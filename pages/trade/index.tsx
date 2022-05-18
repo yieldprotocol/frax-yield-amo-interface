4994eabf08ab4d34044f3668e9c6af4dfb9857d4
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-18 13:10:58 -0400

    fix: only mainnet

--- pages/trade/index.tsx
+++ pages/trade/index.tsx
@@ -4,6 +4,8 @@ import TradeWidget from '../../components/trade/TradeWidget';
 import { URLS } from '../../config/chains';
 import { IPoolMap } from '../../lib/protocol/types';
 import { getContracts, getPools } from '../../lib/protocol';
+import yieldEnv from '../../config/yieldEnv';
+import { FRAX_AMO } from '../../constants';
 
 const Trade = ({ pools }: InferGetStaticPropsType<typeof getStaticProps>) => (
   <TradeWidget pools={JSON.parse(pools!) as IPoolMap} />
@@ -13,10 +15,10 @@ export default Trade;
 
 export async function getStaticProps() {
   try {
-    const chainId = 4;
+    const chainId = 1;
     const provider = new JsonRpcProvider(URLS[chainId][0], chainId);
     const contracts = getContracts(provider, chainId);
-    const pools = await getPools(provider, contracts!, chainId, undefined);
+    const pools = await getPools(provider, contracts!, chainId, yieldEnv.addresses[chainId][FRAX_AMO]);
     return { props: { pools: JSON.stringify(pools) } };
   } catch (error) {
     return { notFound: true, props: { pools: undefined } };
