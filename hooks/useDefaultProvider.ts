e9bfe274c6358a97768727965ace2b55ea7fd26b
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-18 13:09:49 -0400

    fix: use mainnet as default

--- hooks/useDefaultProvider.ts
+++ hooks/useDefaultProvider.ts
@@ -1,11 +1,12 @@
 import { ethers } from 'ethers';
 import { useMemo } from 'react';
-import { useNetwork } from 'wagmi';
+// import { useNetwork } from 'wagmi';
 import { URLS } from '../config/chains';
 
 const useDefaultProvider = () => {
-  const { activeChain } = useNetwork();
-  const chainId = activeChain?.id;
+  // const { activeChain } = useNetwork();
+  // const chainId = activeChain?.id;
+  const chainId = 1;
 
   return useMemo(
     () => (chainId ? new ethers.providers.StaticJsonRpcProvider(URLS[chainId][0], chainId) : undefined),
