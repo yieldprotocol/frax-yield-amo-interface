bbff5b53f247b57ede6d717fa04972644a9c91fa
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-18 13:09:55 -0400

    chore: hide

--- components/Account.tsx
+++ components/Account.tsx
@@ -1,9 +1,9 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+// import { ConnectButton } from '@rainbow-me/rainbowkit';
 // import InfoDropdown from './InfoDropdown';
 
 const Account = () => (
   <div className="flex justify-end items-center">
-    <ConnectButton accountStatus="full" />
+    {/* <ConnectButton accountStatus="full" /> */}
     {/* <InfoDropdown /> */}
   </div>
 );
