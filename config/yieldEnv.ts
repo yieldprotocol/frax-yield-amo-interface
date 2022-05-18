3443dff265dcf58cd7e20cda594bfb7fcc91241d
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-18 12:21:49 -0400

    feat: update env with goerli

--- config/yieldEnv.ts
+++ config/yieldEnv.ts
@@ -3,20 +3,12 @@ const yieldEnv = {
     1: {
       Cauldron: '0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867',
       Ladle: '0x6cB18fF2A33e981D1e38A663Ca056c0a5265066A',
-      WrapEtherModule: '0x22768FCaFe7BB9F03e31cb49823d1Ece30C0b8eA',
+      FraxAMO: '',
     },
-    4: {
-      Cauldron: '0x84EFA55faA9d774B4846c7a51c1C470232DFE50f',
-      Ladle: '0xAE53c79926cb960feA17aF2369DE10938f5D0d52',
-      WrapEtherModule: '0x6C688D4257204FEe0Fce47d3feb9AE81e8B86283',
-    },
-    42: {
-      Cauldron: '0x82Dfaef826C8c190E9b472896a9a99165Ccee35a',
-      Ladle: '0xe7bae0445B9a1DBE834a7379fbE23C6d2Bd61C59',
-    },
-    421611: {
-      Cauldron: '0x64BA0F1D2E5479BF132936328e8c533c95646fE8',
-      Ladle: '0xfdf43681C87Fcb0F9B601902CC668D7E6291Ce41',
+    5: {
+      Cauldron: '0xF39bf75997176915a117Bb274eF6F20784B91568',
+      Ladle: '0xE34989E754e6fF29A0bcbe0A9b0ea818C93bff05',
+      FraxAMO: '',
     },
   },
   seasonColors: {
