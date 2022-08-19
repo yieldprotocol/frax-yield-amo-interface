9fa49704b5444b88334438698b2c521af6f98a83
marcomariscal <marco.a.mariscal@gmail.com>
2022-08-19 16:38:53 -0700

    fix: checking undefined

--- hooks/protocol/useAMO.ts
+++ hooks/protocol/useAMO.ts
@@ -1,13 +1,27 @@
+import { useEffect, useState } from 'react';
 import { useNetwork } from 'wagmi';
 import yieldEnv from '../../config/yieldEnv';
 import { FRAX_AMO } from '../../constants';
+import { AMO } from '../../contracts/types';
 import useContracts from './useContracts';
 
 const useAMO = () => {
   const contracts = useContracts();
   const { chain } = useNetwork();
-  const amoContract = contracts![FRAX_AMO] ?? undefined;
-  const amoAddress = yieldEnv.addresses[chain?.id!].FraxAMO as string;
+  const [amoContract, setAmoContract] = useState<AMO>();
+  const [amoAddress, setAmoAddress] = useState<string>();
+
+  useEffect(() => {
+    if (!contracts) return;
+    const amoContract = contracts[FRAX_AMO] as AMO;
+    if (amoContract) setAmoContract(amoContract);
+  }, [contracts]);
+
+  useEffect(() => {
+    if (!chain?.id) return;
+    const amoAddress = yieldEnv.addresses[chain.id][FRAX_AMO] as string;
+    if (amoAddress) setAmoAddress(amoAddress);
+  }, [chain?.id]);
 
   return {
     amoContract,
--- hooks/protocol/usePools.ts
+++ hooks/protocol/usePools.ts
@@ -1,20 +1,19 @@
 import useSWR from 'swr';
 import { useNetwork } from 'wagmi';
-import yieldEnv from '../../config/yieldEnv';
-import { FRAX_AMO } from '../../constants';
 import { getPools } from '../../lib/protocol';
 import { IPoolMap } from '../../lib/protocol/types';
 import useDefaultProvider from '../useDefaultProvider';
+import useAMO from './useAMO';
 import useContracts from './useContracts';
 
 const usePools = () => {
   const { chain } = useNetwork();
-  const amoAddress = yieldEnv.addresses[chain?.id!][FRAX_AMO] as string;
+  const { amoAddress } = useAMO();
   const provider = useDefaultProvider();
   const contractMap = useContracts();
 
   const { data, error } = useSWR(
-    provider ? `/pools/${chain?.id}/${amoAddress}` : null,
+    provider && chain?.id && amoAddress ? `/pools/${chain?.id}/${amoAddress}` : null,
     () => getPools(provider!, contractMap!, chain?.id, amoAddress),
     {
       revalidateOnFocus: false,
