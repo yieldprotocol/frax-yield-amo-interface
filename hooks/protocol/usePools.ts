a9a9a4121dc2d47ce78e8255b7026ce4a18d38fb
marcomariscal <marco.a.mariscal@gmail.com>
2022-10-18 19:37:34 -0700

    fix: optis

--- hooks/protocol/usePools.ts
+++ hooks/protocol/usePools.ts
@@ -1,4 +1,5 @@
-import useSWR from 'swr/immutable';
+import { useEffect } from 'react';
+import useSWR from 'swr';
 import { useNetwork } from 'wagmi';
 import { getPools } from '../../lib/protocol';
 import { IPoolMap } from '../../lib/protocol/types';
@@ -15,10 +16,29 @@ const usePools = () => {
   const contractMap = useContracts(provider);
   const tenderlyContractMap = useContracts(tenderlyProvider!);
 
-  const { data, error } = useSWR(`/pools?chainId=${chainId}&usingTenderly=${usingTenderly}`, () =>
-    getPools(provider, contractMap!, chainId, usingTenderly, tenderlyContractMap, tenderlyStartBlock)
+  useEffect(() => {
+    console.log('🦄 ~ file: usePools.ts ~ line 13 ~ usePools ~ chainId', chainId);
+  }, [chainId]);
+
+  useEffect(() => {
+    console.log('🦄 ~ file: usePools.ts ~ line 16 ~ usePools ~ usingTenderly', usingTenderly);
+  }, [usingTenderly]);
+
+  useEffect(() => {
+    console.log('🦄 ~ file: usePools.ts ~ line 17 ~ usePools ~ contractMap', contractMap);
+  }, [contractMap]);
+
+  useEffect(() => {
+    console.log('🦄 ~ file: usePools.ts ~ line 18 ~ usePools ~ tenderlyContractMap', tenderlyContractMap);
+  }, [tenderlyContractMap]);
+
+  const { data, error } = useSWR(
+    `/pools?chainId=${chainId}&usingTenderly=${usingTenderly}`,
+    () => getPools(provider, contractMap!, chainId, usingTenderly, tenderlyContractMap, tenderlyStartBlock),
+    { revalidateOnFocus: false, revalidateOnMount: false }
   );
 
+  console.log('🦄 ~ file: usePools.ts ~ line 19 ~ usePools ~ data', data);
   return {
     data: data as IPoolMap | undefined,
     loading: !data && !error,
