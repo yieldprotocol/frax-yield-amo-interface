171c6c39e8484b9b59a7d5b450195a4712596a8c
marcomariscal <marco.a.mariscal@gmail.com>
2022-10-28 15:11:02 -0700

    fix: clean

--- pages/series/index.tsx
+++ pages/series/index.tsx
@@ -1,5 +1,4 @@
 import tw from 'tailwind-styled-components';
-import Link from 'next/link';
 import dynamic from 'next/dynamic';
 
 const DynamicPools = dynamic(() => import('../../components/pool/Pools'), { ssr: false });
@@ -7,7 +6,6 @@ const DynamicPools = dynamic(() => import('../../components/pool/Pools'), { ssr:
 const Container = tw.div`text-center align-middle justify-center`;
 const Inner = tw.div`m-4 text-center`;
 const Header = tw.div`text-lg justify-items-start align-middle`;
-// const Button = tw.button`w-full bg-primary-700/80 align-middle px-4 py-2 text-gray-50 rounded-md hover:bg-primary-800/50`;
 const InnerWrap = tw.div`flex gap-10`;
 const Wrap = tw.div`mx-auto min-w-md p-2 shadow-md rounded-xl dark:bg-black/80 bg-gray-100 dark:text-gray-50`;
 
@@ -18,9 +16,6 @@ const Pool = () => (
         <Inner>
           <div className="flex justify-between align-middle gap-10 items-center">
             <Header>Available Series</Header>
-            <Link href="/pool/add" passHref>
-              <div className="w-30">{/* <Button>+New Position</Button> */}</div>
-            </Link>
           </div>
         </Inner>
         <DynamicPools />
