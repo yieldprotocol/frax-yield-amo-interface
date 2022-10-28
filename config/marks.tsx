93d21a526a36caa09583a9cdd82caf9bdf10afcf
marcomariscal <marco.a.mariscal@gmail.com>
2022-10-28 15:44:33 -0700

    fix: remove unused logos

--- config/marks.tsx
+++ config/marks.tsx
@@ -1,7 +1,4 @@
-import DAIMark from '../components/common/logos/DAIMark';
-import ETHMark from '../components/common/logos/ETHMark';
 import FRAXMark from '../components/common/logos/FRAXMark';
-import USDCMark from '../components/common/logos/USDCMark';
 
 interface IMark {
   component: JSX.Element;
@@ -9,8 +6,5 @@ interface IMark {
 }
 
 export const marks: { [symbol: string]: IMark } = {
-  DAI: { component: <DAIMark key="DAI" />, color: '#F5AC37' },
-  ETH: { component: <ETHMark key="ETH" />, color: '#627EEA' },
-  USDC: { component: <USDCMark key="USDC" />, color: '#3E73C4' },
   FRAX: { component: <FRAXMark key="FRAX" />, color: '#000000' },
 };
