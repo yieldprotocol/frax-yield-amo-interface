b63b76fd165efa3d3a4e393202ceea843eba769e
marcomariscal <marco.a.mariscal@gmail.com>
2022-10-28 08:42:35 -0700

    feat: use wagmi to fetch pools (prelim implementation)

--- hooks/protocol/usePools.ts
+++ hooks/protocol/usePools.ts
@@ -1,11 +1,15 @@
-import { useEffect, useMemo } from 'react';
-import useSWR from 'swr';
-import { useNetwork } from 'wagmi';
-import { getPools } from '../../lib/protocol';
+import _ from 'lodash';
+import { ReadContractsContract } from '@wagmi/core/dist/declarations/src/actions/contracts/readContracts';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useContractReads, useNetwork } from 'wagmi';
+import { FRAX_ADDRESS, LADLE } from '../../constants';
+import { ERC20Permit__factory, Ladle, Pool, Pool__factory } from '../../contracts/types';
+import { getPoolAddresses } from '../../lib/protocol';
 import { IPoolMap } from '../../lib/protocol/types';
 import useDefaultProvider from '../useDefaultProvider';
 import useTenderly from '../useTenderly';
 import useContracts from './useContracts';
+import { Interface } from 'ethers/lib/utils';
 
 const usePools = () => {
   const { chain } = useNetwork();
@@ -16,29 +20,85 @@ const usePools = () => {
   const contractMap = useContracts(provider);
   const tenderlyContractMap = useContracts(tenderlyProvider!);
 
-  const key = `/pools?chainId=${chainId}&usingTenderly=${usingTenderly}`;
+  const [poolAddresses, setPoolAddresses] = useState<string[]>();
+  const [contracts, setContracts] = useState<ReadContractsContract[]>([]);
 
-  const { data, error } = useSWR(
-    key,
-    () =>
-      getPools(
-        provider,
-        contractMap!,
-        chainId,
-        usingTenderly,
-        tenderlyContractMap,
-        tenderlyStartBlock,
-        tenderlyProvider
-      ),
-    {
-      revalidateOnFocus: false,
-      dedupingInterval: 3_600_000, // dont duplicate a request w/ same key for 1hr
+  const _getPoolAddresses = useCallback(async () => {
+    // get all pool addresses from current chain and tenderly (if using)
+    let poolAddresses: string[];
+
+    const ladle = contractMap![LADLE] as Ladle;
+    poolAddresses = await getPoolAddresses(ladle);
+
+    if (usingTenderly && tenderlyContractMap && tenderlyStartBlock) {
+      const tenderlyLadle = tenderlyContractMap[LADLE] as Ladle;
+
+      const poolSet = new Set([...poolAddresses, ...(await getPoolAddresses(tenderlyLadle, tenderlyStartBlock))]);
+
+      poolAddresses = Array.from(poolSet.values());
+      const filtered = await poolAddresses.reduce(async (acc: Promise<string[]>, addr) => {
+        const contract = Pool__factory.connect(addr, usingTenderly ? tenderlyProvider : provider);
+        const base = (await contract.base()).toLowerCase();
+        return base === FRAX_ADDRESS ? [...(await acc), addr] : acc;
+      }, Promise.resolve([]));
+
+      return filtered;
     }
+  }, [contractMap, provider, tenderlyContractMap, tenderlyProvider, tenderlyStartBlock, usingTenderly]);
+
+  useEffect(() => {
+    (async () => {
+      setPoolAddresses(await _getPoolAddresses());
+    })();
+  }, [_getPoolAddresses]);
+
+  const funcs = useMemo(() => ['name', 'maturity', 'fyToken', 'symbol', 'base'], []);
+
+  const calls = useCallback(
+    (addressOrName: string, contractInterface: Interface) =>
+      funcs.map((functionName) => ({
+        addressOrName,
+        contractInterface,
+        functionName,
+      })),
+    [funcs]
   );
+
+  useEffect(() => {
+    if (!poolAddresses) return;
+
+    const _contracts = poolAddresses.reduce((acc, addr) => {
+      const contractInterface = Pool__factory.connect(addr, usingTenderly ? tenderlyProvider : provider).interface;
+
+      return [...acc, ...calls(addr, contractInterface)];
+    }, [] as ReadContractsContract[])!;
+
+    setContracts(_contracts);
+  }, [calls, poolAddresses, provider, tenderlyProvider, usingTenderly]);
+
+  const { data, error, isLoading } = useContractReads({
+    contracts: contracts,
+    enabled: !!contracts.length,
+  });
+
+  const _data = useMemo(() => {
+    if (!poolAddresses) return;
+
+    const chunked = _.chunk(data, funcs.length);
+    const poolData = chunked.reduce((acc, c, i) => {
+      const [name, maturity, fyToken, symbol, base] = c;
+      const address = poolAddresses[i];
+      return { ...acc, [address]: { name, maturity, fyToken, symbol, address, base } };
+    }, {});
+
+    return poolData;
+  }, [data, funcs.length, poolAddresses]);
+
+  console.log('🦄 ~ file: usePools.ts ~ line 47 ~ usePools ~ data', _data);
 
   return {
     data: data as IPoolMap | undefined,
-    loading: !data && !error,
+    loading: isLoading,
     error,
   };
 };
